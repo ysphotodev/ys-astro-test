6f6dad08d379b4e2237757ec0722928c905501b9
ysphotodev <ysphotodev@gmail.com>
2024-05-26 01:12:20 +0300

    Add files via upload

---
+++ astro.config.mjs
@@ -0,0 +1,32 @@
+import { defineConfig , passthroughImageService } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+
+import tailwind from "@astrojs/tailwind";
+import vercel from '@astrojs/vercel';
+import node from '@astrojs/node';
+
+// https://astro.build/config
+export default defineConfig({
+
+  output: 'server', // Set the output to server for SSR
+  // adapter: node({
+  //   mode: 'standalone' // Options can vary depending on the adapter, refer to the adapter's documentation
+  // }),
+
+  adapter: vercel(),
+  
+
+  image:{
+    service: passthroughImageService(),
+      remotePatterns: [{ protocol: "https" }],
+      domains: ["https://res.cloudinary.com"],
+  },
+
+
+
+
+
+  site: 'https://example.com',
+  integrations: [mdx(), sitemap(), tailwind()]
+});
